c2aea58b58a4a9a2f5c2fe8f34117e1e97f4a451
pfaustLouKy <pbfaus01@louisville.edu>
2024-01-21 22:26:12 -0500

    End of Module 2

--- examples/Lectures/dice_6M_return.py
+++ examples/Lectures/dice_6M_return.py
@@ -1,15 +1,18 @@
+"""Roll a six-sided die 6,000,000 times."""
 import random
-
 def dice(rolls):
+    # face frequency counters
     frequency1 = 0
     frequency2 = 0
     frequency3 = 0
     frequency4 = 0
     frequency5 = 0
     frequency6 = 0
-
-    for roll in range(6000000):
+    results = []
+    # 6,000,000 die rolls
+    for roll in range(rolls):  # note underscore separators
         face = random.randrange(1, 7)
+        # increment appropriate face counter
         if face == 1:
             frequency1 += 1
         elif face == 2:
@@ -22,16 +25,6 @@ def dice(rolls):
             frequency5 += 1
         elif face == 6:
             frequency6 += 1
-
-    print(f'Face{"Frequency":>13}')
-    print(f'{1:>4}{frequency1:>13}')
-    print(f'{2:>4}{frequency2:>13}')
-    print(f'{3:>4}{frequency3:>13}')
-    print(f'{4:>4}{frequency4:>13}')
-    print(f'{5:>4}{frequency5:>13}')
-    print(f'{6:>4}{frequency6:>13}')
-
-    results = []
     results.append(frequency1)
     results.append(frequency2)
     results.append(frequency3)
@@ -39,3 +32,4 @@ def dice(rolls):
     results.append(frequency5)
     results.append(frequency6)
     return results
+    
