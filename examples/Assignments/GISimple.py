86aa6ef8c1ceca7946733350294e85ff9565084a
pfaustLouKy <pbfaus01@louisville.edu>
2024-01-31 21:22:22 -0500

    Second part of assignment

---
+++ examples/Assignments/GISimple.py
@@ -0,0 +1,23 @@
+import random
+
+def GISimple(impSpd=(1, 9), golemSpd=(3, 5), headStart=5, exitPosition=50):
+    imp_position = headStart
+    golem_position = 0
+
+    while imp_position < exitPosition and golem_position < exitPosition:
+        imp_move = random.randint(impSpd[0], impSpd[1])
+        golem_move = random.randint(golemSpd[0], golemSpd[1])
+
+        imp_position += imp_move
+        golem_position += golem_move
+
+        if imp_position >= exitPosition:
+            return True
+        elif golem_position >= imp_position:
+            return False
+
+    # If neither condition is met, the chase is ongoing, but it won't happen in this simple simulation.
+
+# Example usage:
+result = GISimple()
+print(result)
